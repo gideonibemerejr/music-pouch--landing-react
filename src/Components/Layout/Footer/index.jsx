0633088ff9510919808b126346e9f1818200186d
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-19 02:32:29 -0500

    Update footer

--- src/Components/Layout/Footer/index.jsx
+++ src/Components/Layout/Footer/index.jsx
@@ -23,7 +23,41 @@ const Footer = () => {
               </p>
             </div>
           </inner>
+          <inner className=' mb4 text-grid grid-container--content'>
+            <div className='grid-block--title'>
+              <h2 className='f2-ns f3'>Consulting</h2>
+            </div>
+            <div className='grid-block--content flex justify-end '>
+              <p className='text f2-ns f5 measure-wide'>
+                <a
+                  className='link black hover-dark-red'
+                  href='mailto:gideon@musicpouch.us'
+                >
+                  gideon@musicpouch.us
+                </a>
+              </p>
+            </div>
+          </inner>
+          <inner className='mb4 text-grid grid-container--content'>
+            <div className='grid-block--title'>
+              <h2 className='f2-ns f3'>Press</h2>
+            </div>
+            <div className='grid-block--content flex justify-end'>
+              <p className='text f2-ns f5 measure-wide'>
+                <a
+                  href='mailto:jordan@musicpouch.us'
+                  className='link black hover-dark-red'
+                >
+                  jordan@musicpouch.us
+                </a>
+              </p>
+            </div>
+          </inner>
         </article>
+        <div className='flex justify-between items-center nav'>
+          <div className='arrow bl bb bw4 '></div>
+          <div className='arrow br bb bw4 '></div>
+        </div>
       </section>
     </footer>
   )
