db433dd15c7ace43827645230ca53d1ba2d18493
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2019-10-19 01:52:12 -0500

    Add react-router and hook up HomePage in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HomePage } from './Pages'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' />
+        {/* Route to Home Page */}
+        <Route exact path='/' component={HomePage} />
       </Switch>
     </Router>
   )
